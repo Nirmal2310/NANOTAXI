710646e59e11f2cc2812c5f2fbf0745b0e5632c7
Nirmal2310 <nirmalmahar2@gmail.com>
2024-09-11 17:43:03 +0530

    Auto-sync commit: 09/11/2024 17:43:03

--- Scripts/get_active_pores.py
+++ Scripts/get_active_pores.py
@@ -1,17 +1,3 @@
-# from minknow_api.manager import Manager
-
-# # Connect to the Sequencing Run
-
-# client = list(Manager(host='localhost', port=9502).flow_cell_positions())[0].connect()
-
-# # Get Pore Scanning Output
-
-# mux_scan_data = client.acquisition.get_acquisition_info().bream_info.mux_scan_results
-
-# # Print the number of active pores
-
-# pore_counts = mux_scan_data[0].counts.get("single_pore")
-
 from minknow_api.manager import Manager
 
 import pandas as pd
