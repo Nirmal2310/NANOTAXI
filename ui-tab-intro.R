4203b4c86d17f6a96df1007844849e10c83fd113
Nirmal2310 <62856422+Nirmal2310@users.noreply.github.com>
2024-10-19 21:55:25 +0530

    Update ui-tab-intro.R

--- ui-tab-intro.R
+++ ui-tab-intro.R
@@ -1,15 +1,20 @@
 tabPanel(
-div("INTRODUCTION", style="font-size: 12px; font-weight: bold; color: #007B9A;
+  div("INTRODUCTION", style="font-size: 12px; font-weight: bold; color: #007B9A;
              font-family: serif"),
-         fluidRow(
-           column(2, wellPanel(
-             h4("Introduction"),
-             a("Features", href="#Features"),br(),
-             a("Data Formats", href="#DataFormat"),br(),
-             a("Additional Information", href="#help"),br()
-           )
-           ),
-           column(8, includeMarkdown("Tabs/Instructions.md"))
-         )
+  fluidRow(
+    column(2, wellPanel(
+      h4("Introduction"),
+      a("Features", href="#Features"),br(),
+      a("Data Visualization", href="#Visualization"),br(),
+      a("Data Format", href="#DataFormat"),br(),
+      a("Input Data", href="#InputData"),br(),
+      a("Output Data", href="#Output"),br(),
+      a("Additional Information", href="#Help"),br(),
+      a("Author Details", href="#Developer"),br(),
+      a("Citation Details", href="#Citation")
+    )
+    ),
+    column(10, includeMarkdown("Tabs/Instructions.md"))
+  )
   
 )
