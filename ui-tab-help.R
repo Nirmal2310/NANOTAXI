36f298bc174c1052454fdfdf431debc6de337fa3
Nirmal2310 <62856422+Nirmal2310@users.noreply.github.com>
2024-10-19 21:55:54 +0530

    Update ui-tab-help.R

--- ui-tab-help.R
+++ ui-tab-help.R
@@ -4,17 +4,17 @@ tabPanel(
          fluidRow(
            column(2, wellPanel(
              h4("Additonal Information"),
+             a("Installation", href="#installation"),br(),
              a("Input Data", href="#inputdata"),br(),
+             a("Pipeline", href="#algorithm"),br(),
              a("Data Format", href="#dataformat"),br(),
-             a("Output Metadata", href="#outputdata"),br(),
-             a("Visulization", href="#vis"),br(),
-             a("ARG Cohort Analysis", href="#cohortanalysis"),br(),
-             a("ARG Distribution", href="#distributionplot"),br(),
-             a("Alpha Diversity", href="#alphadiversity"),br(),
-             a("Beta Diversity", href="#betadiversity"),br(),
+             a("Output Data", href="#outputdata"),br(),
+             a("Visualization", href="#vis"),br(),
+             a("Real-time Analysis", href="#realtime"),br(),
+             a("Cohort Analysis", href="#cohortanalysis"),br(),
            )
            ),
-           column(8, includeMarkdown("Tabs/additional_information.md"))
+           column(10, includeMarkdown("Tabs/additional_information.md"))
          )
          
 )
