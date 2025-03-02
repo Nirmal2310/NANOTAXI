0da506d431033e8d46513f797bd0c213ded38a12
Nirmal <62856422+Nirmal2310@users.noreply.github.com>
2025-03-02 15:30:27 +0530

    Update kraken_install.sh

--- Installation/kraken_install.sh
+++ Installation/kraken_install.sh
@@ -51,32 +51,6 @@ if [ ! -d DATA ]; then
 	mkdir DATA
 fi
 
-cd DATA
-
-if [ ! -d KRAKEN_DATA ]; then
-                
-        source $path/bin/activate kraken2
-
-        kraken2-build --db KRAKEN_DATA --special greengenes
-
-        cd KRAKEN_DATA
-
-        grep -qF "export KRAKEN_DB=\"$PWD\"" ~/.bashrc || echo "export KRAKEN_DB=\"$PWD\"" >> ~/.bashrc
-
-        source ~/.bashrc
-
-        source $path/bin/activate base
-
-fi
-
-cd $base_dir/DATA/KRAKEN_DATA
-
-grep -qF "export KRAKEN_DB=\"$PWD\"" ~/.bashrc || echo "export KRAKEN_DB=\"$PWD\"" >> ~/.bashrc
-
-source ~/.bashrc
-
-cd $base_dir
-
 if { conda env list | grep "nanofilt";} > /dev/null 2>&1; then
 
         echo "Environment Exist"
@@ -87,7 +61,15 @@ else
 
 fi
 
-cd $base_dir
+if { conda env list | grep "seqkit";} > /dev/null 2>&1; then
+
+        echo "Environment Exist"
+
+else
+
+        conda create --name seqkit --file seqkit.txt
+
+fi
 
 if { conda env list | grep "taxonkit";} > /dev/null 2>&1; then
 
@@ -122,6 +104,58 @@ fi
 cd $base_dir/DATA/TAXONKIT_DATA
 
 grep -qF "export TAXONKIT_DB=\"$PWD\"" ~/.bashrc || echo "export TAXONKIT_DB=\"$PWD\"" >> ~/.bashrc
+
+source ~/.bashrc
+
+cd $base_dir
+
+if [ ! -d KRAKEN_DATA ]; then
+                
+        source $path/bin/activate kraken2
+
+        kraken2-build --download-taxonomy --db KRAKEN_DATA --use-ftp  --skip-maps
+
+ 	wget -c https://www.arb-silva.de/fileadmin/silva_databases/current/Exports/SILVA_138.2_SSURef_NR99_tax_silva_trunc.fasta.gz -O SILVA_138.2_ref.fasta.gz && gunzip SILVA_138.2_ref.fasta.gz
+
+  	source $path/bin/activate taxonkit
+
+   	grep ">" SILVA_138.2_ref.fasta | grep "Bacteria\|Archaea" | sed 's/>//g;s/ /\t/' | sed 's/;/\t/g' | awk -F "\t" '{print $1"\t"$8}' | \
+    		taxonkit name2taxid -i 2 --data-dir $TAXONKIT_DB | awk 'BEGIN{FS="\t";OFS="\t"}{print $1, $1"|kraken:taxid|"$3}' > seq_id_replacement.txt
+
+      	source $path/bin/activate seqkit
+
+       	awk -F "\t" '{print $1}' seq_id_replacement.txt | seqkit faidx -X - SILVA_138.2_ref.fasta | \
+		awk 'BEGIN{RS=">";FS="\n"}NR>1{printf ">%s\n",$1;for (i=2;i<=NF;i++) {gsub(/U/,"T",$i); printf "%s\n",$i}}' | sed '/^$/d' > SILVA_138.2_16S.fasta
+
+  	seqkit replace -p '^(\S+)' -r '{kv}$2' -k seq_id_replacement.txt SILVA_138.2_16S.fasta > SILVA_138.2_16S_kraken2_ready.fasta
+
+   	threads=$(if [ $(nproc) -gt 16 ]; then echo 16; else echo $(nproc) | awk '{print $1/2}' ; fi)
+
+   	source $path/bin/activate kraken2
+
+    	kraken2-build --add-to-library SILVA_138.2_16S_kraken2_ready.fasta --db KRAKEN_DATA
+
+     	grep ">" SILVA_138.2_16S_kraken2_ready.fasta | sed 's/>//g' | awk '{split($1,a,"|"); print $1"\t"a[3]}' > KRAKEN_DATA/seqid2taxid.map
+
+     	kraken2-build --build --db KRAKEN_DATA --threads $threads
+
+      	kraken2-build --clean --db KRAKEN_DATA
+
+       	rm -r SILVA_138.2_16S_kraken2_ready.fasta SILVA_138.2_ref.fasta* SILVA_138.2_16S.fasta* seq_id_replacement.txt
+
+        cd KRAKEN_DATA
+
+        grep -qF "export KRAKEN_DB=\"$PWD\"" ~/.bashrc || echo "export KRAKEN_DB=\"$PWD\"" >> ~/.bashrc
+
+        source ~/.bashrc
+
+        source $path/bin/activate base
+
+fi
+
+cd $base_dir/DATA/KRAKEN_DATA
+
+grep -qF "export KRAKEN_DB=\"$PWD\"" ~/.bashrc || echo "export KRAKEN_DB=\"$PWD\"" >> ~/.bashrc
 
 source ~/.bashrc
 
