b2a3b68a525b162f9d04cd03d7795e60b8a19993
Nirmal <62856422+Nirmal2310@users.noreply.github.com>
2025-03-03 22:38:34 +0530

    Update packages.R

--- packages.R
+++ packages.R
@@ -1,25 +1,31 @@
-required = c("shiny", "shinyBS", "reticulate", "tidyverse", "shinyjs", "DT",
+required_cran = c("shiny", "shinyBS", "reticulate", "tidyverse", "shinyjs", "DT",
 "plotly", "shinyFiles", "markdown", "validate", "stringr", "ggpubr",
 "dendextend", "BiocManager", "vegan", "grid", "gridExtra", "ggsci",
-"scales", "viridis", "circlize", "ggrepel", "devtools", "compositions", "forcats", "formattable")
+"scales", "viridis", "circlize", "ggrepel", "devtools", "compositions",
+"forcats", "formattable", "future", "promises", "ggtext", "FactoMineR",
+"ggforce")
 
-sapply(required, function(x){
+required_bioc <- c("ComplexHeatmap", "ANCOMBC")
+
+sapply(required_cran, function(x){
   if(!require(x, character.only = TRUE)){
     install.packages(x); suppressPackageStartupMessages(library(x,  character.only = TRUE))}
   else{suppressPackageStartupMessages(library(x, character.only = TRUE))}
   }
 )
 
-if(!require("ComplexHeatmap", character.only = TRUE)){
-  BiocManager::install("ComplexHeatmap")
-  suppressPackageStartupMessages(library("ComplexHeatmap", character.only = TRUE))
-}else{
-  suppressPackageStartupMessages(library("ComplexHeatmap", character.only = TRUE))
-}
+sapply(required_bioc, function(x){
+  if(!require(x, character.only = TRUE)){
+    BiocManager::install(x)
+    suppressPackageStartupMessages(library(x, character.only = TRUE))
+  }else{
+    suppressPackageStartupMessages(library(x, character.only = TRUE))
+  }
+})
 
 if(!require("pairwiseAdonis", character.only = TRUE)){
   suppressPackageStartupMessages(library(devtools))
-  install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis")
+  devtools::install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis")
   suppressPackageStartupMessages(library("pairwiseAdonis", character.only = TRUE))
 } else{
   suppressPackageStartupMessages(library("pairwiseAdonis", character.only = TRUE))
