2844918ed79d217ff6efac26cefd30e60b5b4bc0
Nirmal <62856422+Nirmal2310@users.noreply.github.com>
2025-06-06 18:56:28 +0200

    Update emu_install.sh

--- Installation/emu_install.sh
+++ Installation/emu_install.sh
@@ -40,12 +40,12 @@ script_dir=$(dirname "$(readlink -f "$0")")
 
 if { conda env list |  grep "emu"; } > /dev/null 2>&1; then
 
-        conda activate emu && conda list --explicit > _current_env.txt
+        conda list -n emu --explicit > _current_env.txt
 
         if diff -q $script_dir/emu.txt _current_env.txt > /dev/null; then
                 echo "Environment exists and up to date." && rm -r _current_env.txt
         else
-                conda create --name emu --file $script_dir/emu.txt && rm -r _current_env.txt
+                conda create --name emu --file $script_dir/emu.txt -y && rm -r _current_env.txt
 	
   		source $path/bin/activate emu
 	
@@ -71,12 +71,12 @@ fi
 
 if { conda env list | grep "nanofilt";} > /dev/null 2>&1; then
 
-	conda activate nanofilt && conda list --explicit > _current_env.txt
+	conda list -n nanofilt --explicit > _current_env.txt
 
         if diff -q $script_dir/nanofilt.txt _current_env.txt > /dev/null; then
                 echo "Environment exists and up to date." && rm -r _current_env.txt
         else
-                conda create --name nanofilt --file $script_dir/nanofilt.txt && rm -r _current_env.txt
+                conda create --name nanofilt --file $script_dir/nanofilt.txt -y && rm -r _current_env.txt
   		
         fi
 
@@ -128,12 +128,12 @@ cd $base_dir
 
 if { conda env list | grep "taxonkit";} > /dev/null 2>&1; then
 
-        conda activate taxonkit && conda list --explicit > _current_env.txt
+        conda list -n taxonkit --explicit > _current_env.txt
 
         if diff -q $script_dir/taxonkit.txt _current_env.txt > /dev/null; then
                 echo "Environment exists and up to date." && rm -r _current_env.txt
         else
-                conda create --name taxonkit --file $script_dir/taxonkit.txt && rm -r _current_env.txt
+                conda create --name taxonkit --file $script_dir/taxonkit.txt -y && rm -r _current_env.txt
         fi
 
         conda activate base
